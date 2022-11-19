6ecd491b6d808ae2865b5b9791876bcecdf99e2a
pierre <ptchoupe8@gmail.com>
2022-11-18 21:28:52 -0600

    ngnix.sh

---
+++ ngnix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Author: devops student
+#Date 11/18/2022
+#nginx installation
+echo " installation of nginx just wait one min "
+yum update
+yum install nginx -y
+echo " installation is ready to be run "
+~
+~
